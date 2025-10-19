e83140acf44e088b576d4aeceeff5e28df81d855
promotion-pal <promotion.work.pal@gmail.com>
2025-10-19 11:39:18 +1000

    test useMemo

--- src/app/test/opt/page.tsx
+++ src/app/test/opt/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/shared/ui/button";
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 export default function TestOptPage() {
   return (
@@ -13,15 +13,35 @@ export default function TestOptPage() {
 
 function TestUseCallback() {
   const [count, setCount] = useState<number>(0);
+  const [count2, setCount2] = useState<number>(0);
 
-  const calc = () => {
+  const calc = useCallback(() => {
     setCount(count + 1);
-  };
+  }, [count]);
+
+  const calc2 = useCallback(() => {
+    setCount2(count2 + 1);
+  }, [count2]);
 
   return (
     <div className="space-y-3">
-      <p>Счетчик: {count}</p>
-      <Button onClick={calc}>+1</Button>
+      <Button onClick={calc}>Кнопка 1: {count}</Button>
+      <Button onClick={calc2}>Кнопка 2: {count2}</Button>
+    </div>
+  );
+}
+
+function TestUseMemo() {
+  const [number, setNumber] = useState<number>(100);
+
+  const calcValue = useMemo(() => {
+    return number * 10000 + 800 * 80000;
+  }, []);
+
+  return (
+    <div>
+      <p>Значение: {number}</p>
+      <p>Вычесленное значение {calcValue}</p>
     </div>
   );
 }
