1e786965a98d43b3424418b2a992ba54f5ea775d
Ylia <89242171473j@gmail.com>
2025-10-19 11:39:56 +1000

    test useMemo

--- src/app/test/opt/page.tsx
+++ src/app/test/opt/page.tsx
@@ -1,23 +1,47 @@
 "use client";
 
 import { Button } from "@/shared/ui/button";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { number } from "zod";
 
 export default function TestOptPage() {
-  return <div>TestUseCallback</div>;
+  return 
+  <div>
+   <TestUseCallback />
+div>;
 }
 
 function TestUseCallback() {
   const [count, setCount] = useState<number>(0);
+  const [count2, setCount2] = useState<number>(0);
 
-  const calc = () => {
+  const calc = useCallback(() => {
     setCount(count + 1);
-  };
+  }, []);
+
+  const calc2 = useCallback(() => {
+    setCount2(count2 + 1);
+  }, []);
 
   return (
     <div className="space-y-3">
-      <p> Счетчик: {count}</p>
-      <Button onClick={calc}>+1</Button>
+      <Button onClick={calc}>Кнопка 1: {count}</Button>
+      <Button onClick={calc2}>Кнопка 2: {count2}</Button>
     </div>
   );
+}
+
+function TestUseMemo() {
+    const [number, setNumber] = useState<number>(100);
+
+    const calcValue = useMemo(() => {
+return number * 1000 + 800 * 8000
+    },[];
+
+    return (
+        <div>
+            <p> Значение: {number}</p>
+            <p> Вычесленное значение {calcValue}</p>
+        </div>
+    );
 }
